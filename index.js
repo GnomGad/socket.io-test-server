95097602d97d6499830a9dbf3a4613b5d3f67298
Zhenya <Canimore@yandex.com>
2023-02-20 22:30:33 +0300

    update server side

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const io = require("socket.io")(3000, {
         credentials: true,
     },
 });
+const connectedUsers = {};
 
 io.on("connection", (socket) => {
     console.log(socket.id);
+    connectedUsers[socket.id] = socket;
+    socket.on("disconnect", (reason) => {
+        console.log("disconnect ", socket.id);
+        delete connectedUsers[socket.id];
+    });
+    socket.on("client-to-server-message", (data) => {
+        // TODO: вынести это в отдельный модуль
+        let message = "В базе нет такого";
+        let name = "BOT";
+        if (data.message.toLowerCase() === "привет") {
+            message = "Приветствую";
+        } else if (data.message.includes("погод")) {
+            message = "Сегодня облачно, от +5 до -2";
+        }
+        if (connectedUsers.hasOwnProperty(data.id)) {
+            connectedUsers[data.id].emit("server-to-client-message", {
+                name: name,
+                message: message,
+            });
+        }
+    });
 });
